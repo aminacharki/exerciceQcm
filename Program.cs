2f7d8498e7227f180096a5c1a784a42e6094fb16
ELITEBOOK <ELITEBOOK@DESKTOP-7MJDDPC>
2022-11-10 22:06:13 +0100

    tst

--- Program.cs
+++ Program.cs
@@ -11,5 +11,53 @@ namespace exerciceQcm
         static void Main(string[] args)
         {
         }
+     
+    class Evaluation
+    {
+        public string matiére { get; set; } = "matiére";
+        public DateTime Duré { get; set; }
+        public DateTime DateDebut { get; set; }
+        public DateTime DateFin { get; set; }
+        public string Enonce { get; set; }
+
+
+        public Evaluation(string matiére, DateTime datedebut, DateTime datefin)
+        {
+            this.matiére = matiére;
+            this.DateDebut = datedebut;
+            this.DateFin = datefin;
+
+        }
+
+
+        public Evaluation(string enonce)
+        {
+            this.Enonce = enonce;
+        }
+        class question
+        {
+            float notereponse;
+            string enonce;
+        }
+        class Qcm : question
+        {
+            public float Certitude { get; set; } = 0;
+            public Qcm(float certitude, string enonce)
+            {
+                this.Certitude = certitude;
+            }
+
+            }
+
+        }
+
     }
 }
+
+
+
+
+
+
+
+
